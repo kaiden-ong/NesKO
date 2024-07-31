20eddee5bfece54d00f906ed8736182e313d6626
kaiden-ong <kaiden.ong000@gmail.com>
2024-07-31 13:33:24 -0700

    Added some init code

--- NesKO.cpp
+++ NesKO.cpp
@@ -4,22 +4,82 @@
 #include <SDL2/SDL.h>
 using namespace std;
 
+typdef struct {
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} sdl_t;
+
 typedef enum {
-    RUNNING,
-    PAUSED,
-    QUIT
+	RUNNING,
+	PAUSED,
+	QUIT
 } nes_state_t;
 
+typedef enum {
+	uint32_t width;
+	uint32_t height;
+	uint32_t scale;
+	bool outline;
+} config_t;
+
 typedef struct {
-    uint16_t ram[0xFFFF];
-    uint16_t PC; // Program Counter
-    uint8_t stack_pointer; 
-    uint8_t A; // Accumulator
-    uint8_t X; // Index Register
-    uint8_t Y; // Index Register
-    const char *romName;
+	nes_state_t state;
+	uint16_t ram[0xFFFF];
+	uint16_t PC; // Program Counter
+	uint8_t stack_pointer; 
+	uint8_t A; // Accumulator
+	uint8_t X; // Index Register
+	uint8_t Y; // Index Register
+	const char *romName;
 } nes_t;
 
+bool init_config(config_t *config, int argc, char **argv) {
+	config->width = 64;
+	config->height = 32;
+	config->scale = 20;
+	config->outline = true;
+	return true;
+}
+
+bool init_sdl(config_t *config, sdl_t* sdl) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		cout << "SDL Init Error: " << SDL_GetError() << endl;
+		return 1;
+	}
+	sdl->window = SDL_CreateWindow("NesKO", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, config->scale * config->width, config->scale * config->height, SDL_WINDOW_ALLOW_HIGHDPI);
+	if (window == null) {
+		cout << "SDL Window Init Error: " << SDL_GetError() << endl;
+		SDL_Quit();
+		return 1;
+	}
+	sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == null) {
+		cout << "SDL Renderer Init Error: " << SDL_GetError() << endl;
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
+}
+
+bool init_nes(nes_t *nes, const char rom_name[]) {
+	const uint32_t entry = 0x200;
+	nes->ram = {0};
+	nes->state = RUNNING;
+}
+
 int main(int argc, char **argv) {
-    
+    	config_t config = {0};
+    	if (!init_config(&config, argc, argv)) {
+		exit(EXIT_FAILURE);
+	}
+
+	sdl_t sdl = {0};
+	if (!init_sdl(&config, &sdl)) {
+		exit(EXIT_FAILURE);
+	}
+	nes_t nes = {0};
+	const char *rom_name = argv[1];
+	if (!init_nes(nes, rom_name) {
+		exit(EXIT_FAILURE);
+	}
 }
