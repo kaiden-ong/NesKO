1a3451e91c2d9ffd02efb0308694caf816ec0172
ryanngu023 <ryannguyen10023@gmail.com>
2024-08-11 17:45:25 -0700

    add functions to CPU

--- nes/cpu.cpp
+++ nes/cpu.cpp
@@ -13,3 +13,24 @@ bool CPU::initialize() {
     std::cout << "CPU init done..." << std::endl;
     return true;
 }
+
+void CPU::execute() {}
+
+uint8_t mem_read(uint16_t addr) {
+    if(addr <= 0x1FFF) {
+        // 2KB internal RAM, section mirrored 3 times
+        return ram[addr & 0x07FF];
+    }
+}
+
+uint16_t mem_read16(uint16_t addr) {}
+
+uint16_t CPU::handleAddressingMode(addressing_mode_t mode) {
+    switch(mode) {
+        case Immediate:
+            return PC++;
+        case Implied:
+            return 0;
+
+    }
+}
--- nes/cpu.h
+++ nes/cpu.h
@@ -1,6 +1,23 @@
 #pragma once
 #include <cstdint>
 
+typedef enum {
+    ZeroPage,
+    ZeroPageX,
+    ZeroPageY,
+    Absolute,
+    AbsoluteX,
+    AbsoluteY,
+    Indirect,
+    IndirectX,
+    IndirectY,
+    Accumlator,
+    Relative,
+    Implied,
+    Immediate,
+    None
+} addressing_mode_t;
+
 class CPU {
     public:
         uint16_t PC; // Program Counter
@@ -16,4 +33,17 @@ class CPU {
 
         CPU(); // create a cpu object
         bool initialize(); // handle cpu initialize functionality like setting registers
+        void execute(); // executes an instruction
+
+        uint8_t mem_read(uint16_t addr);
+        uint16_t mem_read16(uint16_t addr);
+
+        // interrupts
+        // void NMI_req();
+        // void IRQ_req();
+
+        // addressing
+        uint16_t handleAddressingMode(addressing_mode_t mode);
+
+
 };
